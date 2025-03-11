293108691ca8f3746d7981cbb6ff6b16a93dbb13
gh-ita <hatimi.ghita@ensam-casa.ma>
2025-03-11 10:36:47 +0000

    refactored the code to automate the image folders creation and the resizing of the images to the model's adequate size

--- model.py
+++ model.py
@@ -1,29 +1,46 @@
 from inference_sdk import InferenceHTTPClient
 from PIL import Image, ImageDraw, ImageFont
-import json
+import json, os
+from resize_image import resize_with_padding
+
 """
-Safety vest, No hardhat, gloves, Mask, vehicle
+Classes : Safety vest, NO-Safety Vest, Hardhat, 
+No Hardhat, Gloves, Mask, No-Mask, Person, Safety Cone
 """
-image_path = "resized_test_image_2.jpg"
+
+image_folder = "test/"
+result_folder = "result/"
+if not os.path.exists(image_folder) :
+    os.makedirs(image_folder)
+if not os.path.exists(result_folder) :
+    os.makedirs(result_folder)
+    
+image_path = os.path.join(image_folder,"resized_test_image_2.jpg")
+result_path = os.path.join(result_folder,"resized_test_image_2_inference.jpg")
 CLIENT = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
     api_key="AonDlnwLMihldiyHw3P9"
 )
-
+resized_img = resize_with_padding(image_path, image_path)
 results = CLIENT.infer(image_path, model_id="construction-site-safety/27")
 print(json.dumps(results, indent=4))
-#Draw the results on the image 
 image = Image.open(image_path)
 draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
 
 for result in results["predictions"] :
     x, y, width, height = result['x'], result['y'], result['width'], result['height']
+    label = f"{result['class']} {result['confidence']*100}"
     x1 = x - width / 2
     y1 = y - height / 2
     x2 = x1 + width
     y2 = y1 + height
+    bbox = draw.textbbox((x1, y2 - 20), label, font=font)
+    text_width, text_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
     draw.rectangle([x1, y1, x2, y2], outline="red", width=2)
+    draw.rectangle([x1, y2 - text_height, x1 + text_width, y2], fill="white")
+    draw.text((x1, y2 - text_height), label, fill="black", font=font)
+    
 image.show()
-image.save(f"resized_test_image_2_inference.jpg")
+image.save(result_path)
 
--- resize_image.py
+++ resize_image.py
@@ -1,23 +1,12 @@
-from PIL import Image, ImageOps
+from PIL import Image
 
-
-image_path = "test_hat_image_1.jpg"
 def resize_with_padding(image_path, output_path, target_size=(1080, 720)):
     image = Image.open(image_path).convert("RGB")
-
-    # Resize while keeping aspect ratio
     image.thumbnail(target_size, Image.LANCZOS)
-
-    # Create a new image with the target size and white background
-    new_image = Image.new("RGB", target_size, (255, 255, 255))  # White background
-
-    # Paste the resized image in the center
+    new_image = Image.new("RGB", target_size, (255, 255, 255))
     x_offset = (target_size[0] - image.size[0]) // 2
     y_offset = (target_size[1] - image.size[1]) // 2
     new_image.paste(image, (x_offset, y_offset))
-
     new_image.save(output_path)
     print(f"Saved resized image: {output_path}")
 
-# Example usage
-resize_with_padding(image_path, "resized_test_image_2.jpg")
