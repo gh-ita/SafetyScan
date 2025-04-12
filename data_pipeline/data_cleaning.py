266cb2b11d81adfbd0edfb63405e59bb7e5c4f80
gh-ita <hatimi.ghita@ensam-casa.ma>
2025-04-12 15:45:10 +0100

    no redundant images found in the dataste

--- data_pipeline/data_cleaning.py
+++ data_pipeline/data_cleaning.py
@@ -5,14 +5,21 @@
 2- store the empty label files names along with their index 
 3- erase the images and labels 
 """
+"""
+Find redundant data :
+analyse the images arrays 
+"""
 import os
 from PIL import Image
 import matplotlib.pyplot as plt 
+import numpy as np 
+import hashlib
 
 IMAGE_FOLDER = "Construction-Site-Safety/data/images"
 LABEL_FOLDER = "Construction-Site-Safety/data/labels"
 IMG_FILE_LIST = sorted(os.listdir(IMAGE_FOLDER))
 LBL_FILE_LIST = sorted(os.listdir(LABEL_FOLDER))
+print(len(IMG_FILE_LIST))
 
 def empty_label_finder(label_folder_path):
     """
@@ -37,7 +44,7 @@ def check_image_label_index_similarity(label_folder_path, image_folder_path):
             same_order = False
     print(same_order)
     
-def remove_empty_data(file_name_list, img_list, label_list, img_folder, label_folder) :
+def remove_data(file_name_list, img_list, label_list, img_folder, label_folder) :
     """
     Removes the images and labels of the data that doesn't contain any annotations
     file_name_list : list of the files to erase
@@ -50,13 +57,32 @@ def remove_empty_data(file_name_list, img_list, label_list, img_folder, label_fo
         os.remove(os.path.join(img_folder, img_name))
         os.remove(os.path.join(label_folder, label_name))
     print(f"Removed {len(file_name_list)} files, new image folder size {len(os.listdir(img_folder))}, new label folder size {len(os.listdir(label_folder))}")
- 
-#check_image_label_index_similarity(LABEL_FOLDER, IMAGE_FOLDER)
+
+def find_redundant_images(img_list, img_folder):
+    img_instances = {}
+    for img_filename in img_list:
+        img = Image.open(os.path.join(img_folder, img_filename))
+        hist = np.array(img.convert('L').histogram())
+        hist_tuple = tuple(hist) 
+        img_hash = hashlib.md5(np.array(hist_tuple).tobytes()).hexdigest()
+        if img_hash not in img_instances :
+            img_instances[img_hash] = [img_filename]
+        else :
+            img_instances[img_hash].append(img_filename)
+    return img_instances
+
+redundancy_list = find_redundant_images(IMG_FILE_LIST, IMAGE_FOLDER)
+count = 0
+for img_list in redundancy_list.values():
+    if len(img_list) > 1 :
+        count += 1
+print(count)
+
+"""check_image_label_index_similarity(LABEL_FOLDER, IMAGE_FOLDER)
 empty_labels = empty_label_finder(LABEL_FOLDER)
-remove_empty_data(empty_labels, IMG_FILE_LIST, LBL_FILE_LIST, IMAGE_FOLDER, LABEL_FOLDER)
+remove_data(empty_labels, IMG_FILE_LIST, LBL_FILE_LIST, IMAGE_FOLDER, LABEL_FOLDER)
 
-
-"""grid_size = (20,10)
+grid_size = (20,10)
 fig, axes = plt.subplots(grid_size[0], grid_size[1], figsize=(40, 40))
 axes = axes.flatten()
 count = 0
